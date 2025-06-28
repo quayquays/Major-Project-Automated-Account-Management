d5c06de611e86207356c324c2ccf5745c124cd85
alwaysincris3s <2302471D@Student.tp.edu.sg>
2025-06-28 21:36:18 +0800

    Update dormantui.sh

--- dormantui.sh
+++ dormantui.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root or with sudo."
+    exit 1
+fi
+
 REPORT_DIR="/dormant_reports"
 TMPFILE=$(mktemp)
 EMAIL_CONF="/etc/user_emails.conf"
